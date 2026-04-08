85033b51f97ecc53007be17bdb33be3919aa3341
uitzoliveira-copy <uitzoliveira@gmail.com>
2026-04-08 01:20:04 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: '/',
-
-    plugins: [react(), tailwindcss()],
-    export default defineConfig(({ mode }) => {
-  return {
-    base: '/',
-    plugins: [react(), tailwindcss()],
-  };
-});
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true'
-    },
-  };
+export default defineConfig({
+  base: '/',
+  plugins: [react(), tailwindcss()],
 });
